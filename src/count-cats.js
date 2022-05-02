12078066c958f49d53a60571efa0bf6165796ced
kostyaprozhenik <kostyaprozhenik@gmail.com>
2022-05-02 23:29:33 +0300

    feat: new implement count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,24 +1,10 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-/**
- * Given matrix where you have to find cats by ears "^^"
- *
- * @param {Array<Array>} matrix 
- * @return {Number} count of cats found
- *
- * @example
- * countCats([
- *  [0, 1, '^^'],
- *  [0, '^^', 2],
- *  ['^^', 1, 2]
- * ]) => 3`
- *
- */
 function countCats(arr) {
   let count = 0;
-  arr.forEach((elem) => {
-    if (elem?.includes("^^")) {
-      count++
+  arr.flat().forEach((elem) => {
+    if (elem === "^^") {
+    count++;
   }
   });
   return count;
